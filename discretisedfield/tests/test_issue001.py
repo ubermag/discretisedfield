37684b8b2cac03503570ce9fe11d3a1fd793803e
Hans Fangohr <fangohr@soton.ac.uk>
2016-09-25 21:29:10 +0100

    remove xfail decorator after closing #1

--- discretisedfield/tests/test_issue001.py
+++ discretisedfield/tests/test_issue001.py
@@ -1,9 +1,11 @@
 # test for Issue #1 (https://github.com/joommf/discretisedfield/issues/1)
 
+# This test may catch any warnings/print statements that are
+# unintentionally issued during import.
+
 import subprocess
 import pytest
 
-@pytest.mark.xfail
 def test_matplotlib_warning():
     command = """python -c "import discretisedfield" """
     status, output = subprocess.getstatusoutput(command)
