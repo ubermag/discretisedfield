e9b26abad717d6615ca9b93b4929c80506d696cf
Hans Fangohr <fangohr@soton.ac.uk>
2016-09-25 22:41:20 +0100

    add new test (could have been fixture) to set up matplotlib font cache

--- discretisedfield/tests/test_issue001.py
+++ discretisedfield/tests/test_issue001.py
@@ -6,7 +6,26 @@
 import subprocess
 import pytest
 
+
+
+
+def test_matplotlib_warning_setup():
+    # On travis, matplotlib builds the font cache the first time
+    # we import it. Thus, need to import before we do the actual
+    # test
+    # "UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment."
+
+    command = """python -c "import matplotlib" """
+    status, output = subprocess.getstatusoutput(command)
+    print("output = {}".format(output))
+    assert status == 0
+    if len(output) > 0:
+        assert  'building the font cache' in output
+    pass
+    
+
 def test_matplotlib_warning():
+    
     command = """python -c "import discretisedfield" """
     status, output = subprocess.getstatusoutput(command)
 
