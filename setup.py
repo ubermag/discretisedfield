eedefe8d7eb6f5caccff7fcaea74de02c2f9f2f3
mb1a15 <mb4e10@soton.ac.uk>
2016-09-29 11:36:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 from distutils.core import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
 setup(
-    name='discretisedfield',
-    version='0.1',
-    description='A Python package for analysing and manipulating finite difference vector fields',
+    name="discretisedfield",
+    version="0.1",
+    description=("A Python package for analysing and manipulating "
+                 "discretised fields."),
     long_description=readme,
-    author='Computational Modelling Group',
-    author_email='fangohr@soton.ac.uk',
-    url = 'https://github.com/joommf/discretisedfield',
-    download_url = 'https://github.com/joommf/discretisedfield/tarball/0.1',
-    packages=['discretisedfield',
-              'discretisedfield.util',
-              'discretisedfield.tests'],
-    install_requires=[
-        'matplotlib',
-        'tornado',
-        'ipython',
-        'ipykernel'
-    ],
-    classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-    ]
+    author="Computational Modelling Group",
+    author_email="fangohr@soton.ac.uk",
+    url="https://github.com/joommf/discretisedfield",
+    download_url="https://github.com/joommf/discretisedfield/tarball/0.1",
+    packages=["discretisedfield",
+              "discretisedfield.util",
+              "discretisedfield.tests"],
+    install_requires=["matplotlib",
+                      "tornado",
+                      "ipython",
+                      "ipykernel"],
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 3"]
 )
