933471b2d594615bc58f5a673f7197873e152b71
mb1a15 <mb4e10@soton.ac.uk>
2016-09-29 11:32:17 +0100

    Add dependencies for pip install.

--- setup.py
+++ setup.py
@@ -11,10 +11,16 @@ setup(
     author='Computational Modelling Group',
     author_email='fangohr@soton.ac.uk',
     url = 'https://github.com/joommf/discretisedfield',
-    #download_url = 'https://github.com/joommf/discretisedfield/tarball/0.1',
+    download_url = 'https://github.com/joommf/discretisedfield/tarball/0.1',
     packages=['discretisedfield',
               'discretisedfield.util',
               'discretisedfield.tests'],
+    install_requires=[
+        'matplotlib',
+        'tornado',
+        'ipython',
+        'ipykernel'
+    ],
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
