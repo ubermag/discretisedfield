8581ad3c622db0670b8a6571d41c66bc4ee74d8f
mb1a15 <mb4e10@soton.ac.uk>
2016-10-11 14:34:00 +0100

    Tag version.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.rst") as f:
 
 setup(
     name="discretisedfield",
-    version="0.5.2",
+    version="0.5.3",
     description=("A Python package for analysing and manipulating "
                  "discretised fields."),
     long_description=readme,
     author="Computational Modelling Group",
     author_email="fangohr@soton.ac.uk",
     url="https://github.com/joommf/discretisedfield",
-    download_url="https://github.com/joommf/discretisedfield/tarball/0.5.2",
+    download_url="https://github.com/joommf/discretisedfield/tarball/0.5.3",
     packages=["discretisedfield",
               "discretisedfield.util",
               "discretisedfield.tests"],
