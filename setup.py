dd529a0a24ca7d6cdb9794e00d45b16acfd798bc
Marijan Beg <m.beg@soton.ac.uk>
2017-03-30 14:22:56 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setuptools.setup(
     name="discretisedfield",
-    version="0.7",
+    version="0.7.1",
     description=("A Python package for analysing and manipulating "
                  "discretised fields."),
     long_description=readme,
