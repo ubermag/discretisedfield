447924155f368c14c6bb430e30a93f34921f56f4
mb1a15 <m.beg@soton.ac.uk>
2017-05-10 13:08:10 +0100

    Use numpy.array_equiv for simpler comparison.

--- discretisedfield/field.py
+++ discretisedfield/field.py
@@ -183,8 +183,7 @@ class Field(dfu.Field):
 
     @property
     def norm(self):
-        if np.array_equal(self.norm_array[..., None],
-                          self._as_array(self._norm, dim=1)):
+        if np.array_equiv(self.norm_array, self._as_array(self._norm, dim=1)):
             return self._norm
         else:
             return self.norm_array
