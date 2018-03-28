a0257e01ea02ef7cc5c92c2adf118198bba4be28
Marijan Beg <marijan.beg@xfel.eu>
2018-03-28 15:02:29 +0200

    Temporarily disable long description.

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-import codecs
 import setuptools
 
-with codecs.open("README.md") as f:
-    readme = f.read()
+#with open("README.md") as f:
+#    readme = f.read()
 
 setuptools.setup(
     name="discretisedfield",
     version="0.8",
     description=("A Python package for analysing and manipulating "
                  "discretised fields."),
-    long_description=readme,
+    #long_description=readme,
     url='https://joommf.github.io',
     author='Marijan Beg, Ryan A. Pepper, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
