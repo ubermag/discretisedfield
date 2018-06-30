90a9c4fc6bc7834818c3cfca8b66654ec71097d8
Marijan Beg <m.beg@soton.ac.uk>
2018-06-30 19:01:52 +0200

    Update setup.py for Markdown README.

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
 import setuptools
 
-with open("README.md", 'r+', encoding="utf-8") as f:
-    readme = f.read()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name="discretisedfield",
-    version="0.8",
-    description=("Python package for creation, reading, analysis, "
-                 "and plotting of finite difference fields."),
-    long_description=readme,
+    name='discretisedfield',
+    version='0.8',
+    description=('Python package for creation, reading, analysis, '
+                 'and plotting of finite difference fields.'),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://joommf.github.io',
     author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
-    install_requires=["joommfutil",
-                      "matplotlib",
-                      "pytest",
-                      "pyvtk"],
+    install_requires=['joommfutil',
+                      'matplotlib',
+                      'pyvtk'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
                  'Topic :: Scientific/Engineering :: Physics',
                  'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 3 :: Only']
+                 'Natural Language :: English']
 )
