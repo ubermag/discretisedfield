995dc4faef1d664d09c3dd8a0a7828898266eef2
Marijan Beg <marijan.beg@xfel.eu>
2018-07-01 17:07:39 +0200

    Add version and dependencies.

--- discretisedfield/__init__.py
+++ discretisedfield/__init__.py
@@ -1,3 +1,4 @@
+import pkg_resources
 import matplotlib as mpl
 mpl.use("agg")
 from .mesh import Mesh
@@ -8,3 +9,6 @@ from .read import read
 def test():
     import pytest  # pragma: no cover
     return pytest.main(["-v", "--pyargs", "discretisedfield"])  # pragma: no cover
+
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
