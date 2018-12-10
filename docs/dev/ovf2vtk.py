d8f56a52a2fd7e1a83b15ade4a3e2cf4a76c6dcc
Sergii Mamedov <finve.ua@gmail.com>
2018-12-10 11:04:22 +0100

    First simple version of ovf2vtk

---
+++ docs/dev/ovf2vtk.py
@@ -0,0 +1,42 @@
+import argparse
+import discretisedfield as df
+
+
+def convert_files(inputs, outputs):
+
+    for in_, out_ in zip(inputs, outputs):
+        field = df.read(in_)
+        field.write(out_)
+
+def main():
+    
+    parser = argparse.ArgumentParser(
+        prog='ovf2vtk',
+        description='ovf2vtk -- data conversion from OOMMF`s omf/ovf file format to VTK.'
+    )
+    parser.add_argument('--infile', type=argparse.FileType('r'), nargs='+', required=True, 
+        help='One or more input file')
+    parser.add_argument('--outfile', type=argparse.FileType('w'), nargs='+',
+        help='One or more output file, optional')
+    args = parser.parse_args()
+
+    # check name of output file
+    if args.outfile:
+        
+        # check count input and output files
+        if (len(args.infile) == len(args.outfile)):
+            input_files = [file.name for file in args.infile]
+            output_files = [file.name for file in args.outfile]
+        else:
+            print('\nError: The number of input and output files does not match')
+            return
+    else:
+        input_files = [file.name for file in args.infile]
+        output_files = ['{}.vtk'.format(file.split('.')[0])
+                            for file in input_files]
+
+    convert_files(input_files, output_files)
+
+
+if __name__ == "__main__":
+    main()
