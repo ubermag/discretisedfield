e4e65c65ea161933c3c7cdaaff79927921bb35ad
Marijan Beg <marijan.beg@xfel.eu>
2019-08-08 19:37:23 +0200

    Add test for plane_info.

---
+++ discretisedfield/tests/test_util.py
@@ -0,0 +1,61 @@
+import pytest
+import discretisedfield.util as dfu
+
+
+def test_plane_info():
+    info = dfu.plane_info('x')
+    assert info['planeaxis'] == 0
+    assert info['axis1'] == 1
+    assert info['axis2'] == 2
+    assert info['point'] is None
+
+    info = dfu.plane_info('y')
+    assert info['planeaxis'] == 1
+    assert info['axis1'] == 0
+    assert info['axis2'] == 2
+    assert info['point'] is None
+
+    info = dfu.plane_info('z')
+    assert info['planeaxis'] == 2
+    assert info['axis1'] == 0
+    assert info['axis2'] == 1
+    assert info['point'] is None
+
+    info = dfu.plane_info(x=0)
+    assert info['planeaxis'] == 0
+    assert info['axis1'] == 1
+    assert info['axis2'] == 2
+    assert info['point'] == 0
+
+    info = dfu.plane_info(y=0)
+    assert info['planeaxis'] == 1
+    assert info['axis1'] == 0
+    assert info['axis2'] == 2
+    assert info['point'] == 0
+
+    info = dfu.plane_info(z=0)
+    assert info['planeaxis'] == 2
+    assert info['axis1'] == 0
+    assert info['axis2'] == 1
+    assert info['point'] == 0
+
+    info = dfu.plane_info(x=5)
+    assert info['planeaxis'] == 0
+    assert info['axis1'] == 1
+    assert info['axis2'] == 2
+    assert info['point'] == 5
+
+    with pytest.raises(ValueError):
+        info = dfu.plane_info('xy')
+    with pytest.raises(ValueError):
+        info = dfu.plane_info('zy')
+    with pytest.raises(ValueError):
+        info = dfu.plane_info('y', 'x')
+    with pytest.raises(ValueError):
+        info = dfu.plane_info('xzy')
+    with pytest.raises(ValueError):
+        info = dfu.plane_info('z', x=3)
+    with pytest.raises(ValueError):
+        info = dfu.plane_info('y', y=5)
+    with pytest.raises(ValueError):
+        info = dfu.plane_info(x=0, z=3)
