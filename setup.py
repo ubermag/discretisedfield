01fd759e52e56c431cb13347b5184a2017d1dd67
Marijan Beg <marijan.beg@xfel.eu>
2019-08-11 16:34:02 +0200

    Tidy up.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
                       'matplotlib',
                       'pyvtk',
                       'k3d'],
+    include_package_data = True,
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
                  'Programming Language :: Python :: 3 :: Only',
