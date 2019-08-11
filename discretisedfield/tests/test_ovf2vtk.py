7cec6375a17c37502f8cee7b6ce29a38da1deb47
Marijan Beg <marijan.beg@xfel.eu>
2019-08-11 16:58:36 +0200

    Extend test.

--- discretisedfield/tests/test_ovf2vtk.py
+++ discretisedfield/tests/test_ovf2vtk.py
@@ -5,31 +5,39 @@ import discretisedfield as df
 from discretisedfield.ovf2vtk import convert_files
 
 
+def check_vtk(vtkfile):
+    pattern = ''
+    with open(vtkfile, 'r') as f:
+        for line in f.readlines():
+            if 'CELL_DATA' in line:
+                pattern = line.strip()
+                break
+    assert pattern == 'CELL_DATA 8'
+
 def test_ovf2vtk():
     mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 10), cell=(5, 5, 5))
     f = df.Field(mesh, dim=3, value=(1, 2, -3.14))
 
-    filename_omf = "test_ovf2vtk_file.omf"
-    filename_vtk = "test_ovf2vtk_file.vtk"
-
+    filename_omf = 'test_ovf2vtk_file.omf'
+    filename_vtk = 'test_ovf2vtk_file.vtk'
     f.write(filename_omf)
 
-    convert_files([filename_omf], [filename_vtk])
-
     interpreter = sys.executable.split('/')[-1]
+
+    # No output filename provided.
     cmd = [interpreter, '-m', 'discretisedfield.ovf2vtk',
            '--infile', f'{filename_omf}']
     proc_return = subprocess.run(cmd)
-
     assert proc_return.returncode == 0
+    check_vtk(filename_vtk)
 
-    # Check created VTK file.
-    pattern = ''
-    with open(filename_vtk, 'r') as f:
-        for line in f.readlines():
-            if 'CELL_DATA' in line:
-                pattern = line.strip()
-    assert pattern == 'CELL_DATA 8'
+    # Output filename provided.
+    cmd = [interpreter, '-m', 'discretisedfield.ovf2vtk',
+           '--infile', f'{filename_omf}', '--outfile',
+           f'{filename_vtk}']
+    proc_return = subprocess.run(cmd)
+    assert proc_return.returncode == 0
+    check_vtk(filename_vtk)
 
     os.remove(f'{filename_omf}')
     os.remove(f'{filename_vtk}')
