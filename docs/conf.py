c25851f5b94e56075f09789dce94e0e8062a03a2
Marijan Beg <marijan.beg@xfel.eu>
2019-08-12 19:49:59 +0200

    Add master doc.

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,5 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = 'sphinx_rtd_theme'
+
+master_doc = 'index'
