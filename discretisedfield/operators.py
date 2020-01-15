7db3d90436167e9c4ff5a1c2f2caf252a469baf7
Marijan Beg <m.beg@soton.ac.uk>
2020-01-15 15:46:05 +0000

    Add cross product.

--- discretisedfield/operators.py
+++ discretisedfield/operators.py
@@ -7,3 +7,9 @@ def dot(f1, f2):
     if dfu.compatible(f1, f2) and f1.dim > 1:
         res_array = np.einsum('ijkl,ijkl->ijk', f1.array, f2.array)
         return df.Field(f1.mesh, dim=1, value=res_array[..., np.newaxis])
+
+
+def cross(f1, f2):
+    if dfu.compatible(f1, f2) and f1.dim == 3:
+        res_array = np.cross(f1.array, f2.array)
+        return df.Field(f1.mesh, dim=3, value=res_array)
--- discretisedfield/tests/test_operators.py
+++ discretisedfield/tests/test_operators.py
@@ -41,3 +41,53 @@ def test_dot():
     assert df.dot(f1, f2)((1, 1, 1)) == (3,)
     assert df.dot(f1, f2)((3, 1, 1)) == (7,)
     assert df.dot(f1, f2)((5, 7, 1)) == (47,)
+
+
+def test_cross():
+    p1 = (0, 0, 0)
+    p2 = (10, 10, 10)
+    cell = (2, 2, 2)
+    mesh = df.Mesh(p1=p1, p2=p2, cell=cell)
+
+    # Zero vectors
+    f1 = df.Field(mesh, dim=3, value=(0, 0, 0))
+    res = df.cross(f1, f1)
+    assert res.dim == 3
+    assert res.array.shape == (5, 5, 5, 3)
+    assert res.average == (0, 0, 0)
+
+    # Orthogonal vectors
+    f1 = df.Field(mesh, dim=3, value=(1, 0, 0))
+    f2 = df.Field(mesh, dim=3, value=(0, 1, 0))
+    f3 = df.Field(mesh, dim=3, value=(0, 0, 1))
+    assert df.cross(f1, f2).average == (0, 0, 1)
+    assert df.cross(f1, f3).average == (0, -1, 0)
+    assert df.cross(f2, f3).average == (1, 0, 0)
+    assert df.cross(f1, f1).average == (0, 0, 0)
+    assert df.cross(f2, f2).average == (0, 0, 0)
+    assert df.cross(f3, f3).average == (0, 0, 0)
+
+    # Check if not comutative
+    assert df.cross(f1, f2) == -df.cross(f2, f1)
+    assert df.cross(f1, f3) == -df.cross(f3, f1)
+    assert df.cross(f2, f3) == -df.cross(f3, f2)
+
+    # Spatially varying vectors
+    def value_fun1(pos):
+        x, y, z = pos
+        return (x, y, z)
+
+    def value_fun2(pos):
+        x, y, z = pos
+        return (z, x, y)
+
+    f1 = df.Field(mesh, dim=3, value=value_fun1)
+    f2 = df.Field(mesh, dim=3, value=value_fun2)
+
+    # The cross product should be
+    # (y**2-x*z, z**2-x*y, x**2-y*z)
+    assert df.cross(f1, f2)((1, 1, 1)) == (0, 0, 0)
+    assert df.cross(f1, f2)((3, 1, 1)) == (-2, -2, 8)
+    assert df.cross(f2, f1)((3, 1, 1)) == (2, 2, -8)
+    assert df.cross(f1, f2)((5, 7, 1)) == (44, -34, 18)
+
