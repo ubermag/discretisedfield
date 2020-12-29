c74935c62bf0652ba8a11b3d13798df64999b83f
marijanbeg <m.beg@soton.ac.uk>
2020-12-29 13:28:23 +0000

    Doc string.

--- discretisedfield/tools/__init__.py
+++ discretisedfield/tools/__init__.py
@@ -1,4 +1,4 @@
-"""convenience tools"""
+"""Convenience tools"""
 
 from .tools import neigbouring_cell_angle, max_neigbouring_cell_angle, \
     topological_charge_density, topological_charge, emergent_magnetic_field
