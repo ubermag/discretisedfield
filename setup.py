8f05b8ec8a06cea6fa059b9c4779ae11a12f691a
marijanbeg <m.beg@soton.ac.uk>
2020-12-29 13:41:57 +0000

    Tidy up.

--- setup.py
+++ setup.py
@@ -6,19 +6,14 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setuptools.setup(
     name='discretisedfield',
     version='0.8.13',
-    author=('Marijan Beg, Martin Lang, Ryan A. Pepper, '
-            'Thomas Kluyver, and Hans Fangohr'),
     description=('Python package for definition, reading, '
                  'and visualisation of finite-difference fields.'),
+    author=('Marijan Beg, Martin Lang, Ryan A. Pepper, '
+            'Thomas Kluyver, and Hans Fangohr'),
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://ubermag.github.io',
     packages=setuptools.find_packages(),
-    entry_points={
-        "console_scripts": [
-            "ovf2vtk = discretisedfield.ovf2vtk:main",
-        ],
-    },
     include_package_data=True,
     python_requires='>=3.8',
     install_requires=['ubermagutil==0.2.6',
@@ -27,6 +22,11 @@ setuptools.setup(
                       'jupyterlab==2.2',
                       'h5py>=3.1',
                       'k3d>=2.9'],
+    entry_points={
+        "console_scripts": [
+            "ovf2vtk = discretisedfield.ovf2vtk:main",
+        ],
+    },
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Intended Audience :: Education',
                  'Intended Audience :: Developers',
