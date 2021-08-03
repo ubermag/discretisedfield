c2358ea53f1a1f8a9b9778d28d422efa02a11c10
Martin Lang <m.lang@soton.ac.uk>
2021-08-03 17:57:22 +0200

    Demag tensor and field.

--- discretisedfield/tools/tools.py
+++ discretisedfield/tools/tools.py
@@ -690,3 +690,115 @@ def count_bps(field, /, direction='x'):
     results[f'bp_pattern_{direction}'] = str(pattern)
 
     return results
+
+
+def demag_tensor(mesh):
+    """Fourier transform of the demag tensor.
+
+    Computes the demag tensor in Fourier space. Only the six different
+    components Nxx, Nyy, Nzz, Nxy, Nxz, Nyz are returned.
+
+    Parameters
+    ----------
+    mesh : discretisedfield.Mesh
+        Mesh to compute the demag tensor on.
+
+    Returns
+    -------
+    discretisedfield.Field
+        Demag tensor in Fourier space.
+    """
+    p1 = [(-i + 1) * j - j/2 for i, j in zip(mesh.n, mesh.cell)]
+    p2 = [(i - 1) * j + j/2 for i, j in zip(mesh.n, mesh.cell)]
+    n = [2*i - 1 for i in mesh.n]
+    mesh_new = df.Mesh(p1=p1, p2=p2, n=n)
+
+    return df.Field(mesh_new, dim=6, value=_N(mesh_new)).fftn
+
+
+def demag_field(m, tensor):
+    """Demagnetisation field computed using ...
+
+    Parameters
+    ----------
+    m : discretisedfield.Field
+        Magnetisation field
+
+    tensor : discretisedfield.field
+        Demagnetisation tensor obatained with ``dft.demag_tensor``
+
+    Returns
+    -------
+    discretisedfield.Field
+        Demagnetisation field
+    """
+    m_pad = m.pad({d: (0, m.mesh.n[i] - 1)
+                   for d, i in zip(['x', 'y', 'z'], range(3))},
+                  mode='constant')
+    m_pad_fft = m_pad.fftn
+
+    hx = (tensor.array[..., 0] * m_pad_fft.x.array.squeeze()
+          + tensor.array[..., 3] * m_pad_fft.y.array.squeeze()
+          + tensor.array[..., 4] * m_pad_fft.z.array.squeeze())
+    hy = (tensor.array[..., 3] * m_pad_fft.x.array.squeeze()
+          + tensor.array[..., 1] * m_pad_fft.y.array.squeeze()
+          + tensor.array[..., 5] * m_pad_fft.z.array.squeeze())
+    hz = (tensor.array[..., 4] * m_pad_fft.x.array.squeeze()
+          + tensor.array[..., 5] * m_pad_fft.y.array.squeeze()
+          + tensor.array[..., 2] * m_pad_fft.z.array.squeeze())
+
+    H = df.Field(m_pad_fft.mesh, dim=3,
+                 value=np.stack((hx, hy, hz), axis=3)).ifftn
+    return df.Field(m.mesh, dim=3,
+                    value=H.array[m.mesh.n[0] - 1:, m.mesh.n[1] - 1:,
+                                  m.mesh.n[2] - 1:, :]).real
+
+
+def _f(x, y, z):
+    eps = 1e-18  # to avoid zero division; similar to Albert et al. 2015
+    return (abs(y) / 2 * (z**2 - x**2)
+            * np.arcsinh(abs(y) / (np.sqrt(x**2 + z**2) + eps))
+            + abs(z) / 2 * (y**2 - x**2)
+            * np.arcsinh(abs(z) / (np.sqrt(x**2 + y**2) + eps))
+            - abs(x * y * z)
+            * np.arctan(abs(y * z) /
+                        (abs(x) * np.sqrt(x**2 + y**2 + z**2) + eps))
+            + 1/6 * (2 * x**2 - y**2 - z**2) * np.sqrt(x**2 + y**2 + z**2))
+
+
+def _g(x, y, z):
+    eps = 1e-18  # to avoid zero division; similar to Albert et al. 2015
+    return (x * y * z * np.arcsinh(z / (np.sqrt(x**2 + y**2) + eps))
+            + y / 6 * (3 * z**2 - y**2)
+            * np.arcsinh(x / (np.sqrt(y**2 + z**2) + eps))
+            + x / 6 * (3 * z**2 - x**2)
+            * np.arcsinh(y / (np.sqrt(x**2 + z**2) + eps))
+            - z**3 / 6
+            * np.arctan(x * y / (z * np.sqrt(x**2 + y**2 + z**2) + eps))
+            - z * y**2 / 2
+            * np.arctan(x * z / (y * np.sqrt(x**2 + y**2 + z**2) + eps))
+            - z * x**2 / 2
+            * np.arctan(y * z / (x * np.sqrt(x**2 + y**2 + z**2) + eps))
+            - x * y * np.sqrt(x**2 + y**2 + z**2) / 3)
+
+
+def _N_element(x, y, z, mesh, function):
+    dx, dy, dz = mesh.cell
+    value = 0.
+    for i in np.rollaxis(np.indices((2,) * 6), 0, 7).reshape(64, 6):
+        value += (-1)**np.sum(i) * function(x + (i[0] - i[3]) * dx,
+                                            y + (i[1] - i[4]) * dy,
+                                            z + (i[2] - i[5]) * dz)
+    return - value / (4 * np.pi * np.prod(mesh.cell))
+
+
+def _N(mesh):
+    def _inner(p):
+        x, y, z = p
+        return (_N_element(x, y, z, mesh, _f),  # Nxx
+                _N_element(y, z, x, mesh, _f),  # Nyy
+                _N_element(z, x, y, mesh, _f),  # Nzz
+                _N_element(x, y, z, mesh, _g),  # Nxy
+                _N_element(x, z, y, mesh, _g),  # Nxz
+                _N_element(y, z, x, mesh, _g))  # Nyz
+    return _inner
