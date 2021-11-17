0361db07b6902eb8a84df26eedf50075e88954dd
Martin Lang <m.lang@soton.ac.uk>
2021-11-17 16:02:28 +0100

    Rename submodule and reorder code

--- discretisedfield/html_templates/__init__.py
+++
@@ -1,1 +0,0 @@
-from .html_templates import html_template
--- discretisedfield/html_templates/html_templates.py
+++
@@ -1,9 +0,0 @@
-import jinja2
-import os
-
-
-def html_template(name):
-    """Return html template with the given name."""
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')))
-    return env.get_template(name + '.jinja2')
