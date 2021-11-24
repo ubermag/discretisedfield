66701ffb480a95060acb53661951c27dfe8d3f0f
Martin Lang <m.lang@soton.ac.uk>
2021-11-24 17:01:56 +0100

    Fix keyword argument in front of non keyword argument.

--- discretisedfield/field.py
+++ discretisedfield/field.py
@@ -435,7 +435,7 @@ class Field(collections.abc.Callable):  # could be avoided by using type hints
                 raise ValueError(msg)
 
             self.array /= self.norm.array  # normalise to 1
-            self.array *= dfu.as_array(val=val, self.mesh, dim=1,
+            self.array *= dfu.as_array(val, self.mesh, dim=1,
                                        dtype=self.dtype)
 
     def __abs__(self):
