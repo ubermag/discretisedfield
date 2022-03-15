be7c0f05880b906953d797d9d085fff56b8cf9b3
Martin Lang <m.lang@soton.ac.uk>
2022-03-15 10:57:59 +0100

    Split test for ovf2vtk

--- discretisedfield/tests/test_ovf2vtk.py
+++ discretisedfield/tests/test_ovf2vtk.py
@@ -6,7 +6,7 @@ import numpy as np
 import discretisedfield as df
 
 
-def test_ovf2vtk(tmp_path, capsys):
+def test_ovf2vtk_explicit(tmp_path):
     p1 = (0, 0, 0)
     p2 = (10e-9, 7e-9, 2e-9)
     cell = (1e-9, 1e-9, 1e-9)
@@ -32,6 +32,19 @@ def test_ovf2vtk(tmp_path, capsys):
     f_read = df.Field.fromfile(vtkfilename)
     assert np.allclose(f.array, f_read.array)
 
+
+def test_ovf2vtk_implicit(tmp_path):
+    p1 = (0, 0, 0)
+    p2 = (10e-9, 7e-9, 2e-9)
+    cell = (1e-9, 1e-9, 1e-9)
+    mesh = df.Mesh(p1=p1, p2=p2, cell=cell)
+
+    def value_fun(point):
+        x, y, z = point
+        c = 1e9
+        return c*x, c*y, c*z
+
+    f = df.Field(mesh, dim=3, value=value_fun)
     # Output filename not provided.
     omffilename = 'test-ovf2vtk2.omf'
     vtkfilename = 'test-ovf2vtk2.vtk'
@@ -45,6 +58,8 @@ def test_ovf2vtk(tmp_path, capsys):
     f_read = df.Field.fromfile(vtkfilename)
     assert np.allclose(f.array, f_read.array)
 
+
+def test_ovf2vtk_wrong(capsys):
     # Number of input and output files do not match.
     cmd = [sys.executable, '-m', 'discretisedfield.ovf2vtk',
            '-i', 'file1.omf', 'file2.omf',
