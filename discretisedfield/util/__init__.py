6ebefbcc09fee9b9ea7c997cf3f1fe01216546c7
Martin Lang <m.lang@soton.ac.uk>
2022-03-15 14:26:38 +0100

    Fix missing imports

--- discretisedfield/util/__init__.py
+++ discretisedfield/util/__init__.py
@@ -1,4 +1,3 @@
 from .util import (array2tuple, as_array, assemble_index, axesdict,
                    bergluescher_angle, cp_hex, cp_int, hls2rgb,
-                   normalise_to_range, plot_box, plot_line, raxesdict,
-                   vtk_scalar_data, vtk_vector_data)
+                   normalise_to_range, plot_box, plot_line, raxesdict)
