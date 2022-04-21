c87eab54a82cc44b373e65d477c75a4b451b840e
Martin Lang <m.lang@soton.ac.uk>
2022-04-21 17:04:22 +0200

    Attempt to ignore holoviews output during tests.

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,3 +1,6 @@
 def pytest_collectstart(collector):
     if collector.fspath and collector.fspath.ext == ".ipynb":
-        collector.skip_compare += ("application/javascript",)
+        collector.skip_compare += (
+            "application/javascript",
+            "application/vnd.holoviews_load.v0+json",
+        )
