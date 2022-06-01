6f36c4b7ab8cfc059a9306f613b7a499ea9b1f76
Martin Lang <m.lang@soton.ac.uk>
2022-06-01 12:54:46 +0200

    Define default attributes and their values in one place

--- discretisedfield/plotting/util.py
+++ discretisedfield/plotting/util.py
@@ -1,7 +1,9 @@
 class Defaults:
     """Default settings for plotting."""
 
-    _all = ["norm_filter"]
+    _defaults = {
+        "norm_filter": True,
+    }
 
     def __init__(self, *classes):
         self._classes = classes
@@ -9,27 +11,28 @@ class Defaults:
 
     def reset(self):
         """Reset values to their defaults."""
-        self.norm_filter = True
+        for key, value in self._defaults.items():
+            setattr(self, key, value)
 
     def __repr__(self):
         summary = "plotting defaults\n"
-        for setting in self._all:
+        for setting in self._defaults:
             summary += f"  {setting}: {getattr(self, setting)}\n"
         return summary
 
     def __getattr__(self, attr):
-        if attr not in self._all:
+        if attr not in self._defaults:
             raise AttributeError(
                 f"{self.__class__.__name__!r} object has no attribute {attr!r}."
             )
         return getattr(self, f"_{attr}")
 
     def __setattr__(self, attr, value):
-        if attr in self._all:
+        if attr in self._defaults:
             attr = f"_{attr}"
             for class_ in self._classes:
                 setattr(class_, attr, value)
         super().__setattr__(attr, value)
 
     def __dir__(self):
-        return dir(self.__class__) + self._all
+        return dir(self.__class__) + list(self._defaults)
