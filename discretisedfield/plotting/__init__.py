188c0dc1be1c2dddf7e0c4699b29fcffc9817a06
Martin Lang <m.lang@soton.ac.uk>
2022-06-01 08:22:14 +0200

    Move defaults class to separate plotting util module

--- discretisedfield/plotting/__init__.py
+++ discretisedfield/plotting/__init__.py
@@ -1,4 +1,5 @@
 """Matplotlib based plotting."""
+import discretisedfield.plotting.util as dfp_util
 from discretisedfield.plotting.hv import Hv
 from discretisedfield.plotting.k3d_field import K3dField
 from discretisedfield.plotting.k3d_region import K3dRegion
@@ -6,32 +7,5 @@ from discretisedfield.plotting.mpl import add_colorwheel
 from discretisedfield.plotting.mpl_field import MplField
 from discretisedfield.plotting.mpl_region import MplRegion
 
-
-class _Defaults:
-    """Default settings for plotting."""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        """Reset values to their defaults."""
-        self.norm_filter = True
-
-    def __repr__(self):
-        summary = "plotting defaults\n"
-        summary += f"  norm_filter: {self.norm_filter}"
-        return summary
-
-    @property
-    def norm_filter(self):
-        """Apply automatic norm-based filtering in convenience plotting methods."""
-        return self._norm_filter
-
-    @norm_filter.setter
-    def norm_filter(self, norm_filter):
-        self._norm_filter = norm_filter
-        Hv._norm_filter = norm_filter
-
-
 """Default settings for plotting."""
-defaults = _Defaults()
+defaults = dfp_util.Defaults()
