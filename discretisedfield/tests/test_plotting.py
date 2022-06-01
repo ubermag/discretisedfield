c3cf2ebff03b5e79a4bdc8b7b738b5f985ef70e6
Martin Lang <m.lang@soton.ac.uk>
2022-06-01 13:09:26 +0200

    Extend tests for plotting defaults

--- discretisedfield/tests/test_plotting.py
+++ discretisedfield/tests/test_plotting.py
@@ -1,3 +1,5 @@
+import pytest
+
 import discretisedfield as df
 
 
@@ -7,10 +9,17 @@ def test_defaults():
     assert df.plotting.Hv._norm_filter
     assert repr(df.plotting.defaults) == "plotting defaults\n  norm_filter: True"
 
+    assert list(df.plotting.defaults) == ["norm_filter"]
+    assert all(key in dir(df.plotting.defaults) for key in df.plotting.defaults)
+
+    with pytest.raises(AttributeError):
+        df.plotting.defaults.nonexisting
+
     # disable norm filtering
     df.plotting.defaults.norm_filter = False
     assert not df.plotting.Hv._norm_filter
 
     # enable norm filtering
-    df.plotting.defaults.norm_filter = True
+    df.plotting.defaults.reset()
+    assert df.plotting.defaults.norm_filter
     assert df.plotting.Hv._norm_filter
