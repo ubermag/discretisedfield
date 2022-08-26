507f4a779f7a2140f35d191571fd41a051154827
Sam Holt <s.j.r.holt@soton.ac.uk>
2022-08-26 13:11:13 +0100

    add divide

--- discretisedfield/field_new.py
+++ discretisedfield/field_new.py
@@ -291,8 +291,14 @@ class Field:
             self.mesh, dim=self.nvdims, value=self.data * other, units=self.units
         )
 
-    def __truediv__(self):
-        raise NotImplementedError()
+    def __truediv__(self, other):
+        if isinstance(other, self.__class__):
+            other = other.data
+        # TODO Could have nicer error mesasges for vdims and mesh checking
+
+        return self.__class__(
+            self.mesh, dim=self.nvdims, value=self.data / other, units=self.units
+        )
 
     def __pow__(self):
         raise NotImplementedError()
