66f3c9799e5eb8bfef889950811dd978a10f33f8
Sam Holt <s.j.r.holt@soton.ac.uk>
2022-08-27 09:47:00 +0100

    right dunder methods

--- discretisedfield/field_new.py
+++ discretisedfield/field_new.py
@@ -365,8 +365,8 @@ class Field:
             self.mesh, dim=self.nvdims, value=np.abs(self.data.data), units=self.units
         )
 
-    # def __pos__(self): -> remove and see if tests pass using default behaviour
-    #    return self
+    def __pos__(self):
+        return self
 
     def __neg__(self):
         return -1 * self
@@ -380,6 +380,9 @@ class Field:
             self.mesh, dim=self.nvdims, value=self.data + other, units=self.units
         )
 
+    def __radd__(self, other):
+        return self + other
+
     def __sub__(self, other):
         if isinstance(other, self.__class__):
             self.is_same_mesh_field(other)
@@ -388,6 +391,9 @@ class Field:
         return self.__class__(
             self.mesh, dim=self.nvdims, value=self.data - other, units=self.units
         )
+
+    def __rsub__(self, other):
+        return -self + other
 
     def __mul__(self, other):
         if isinstance(other, self.__class__):
@@ -398,6 +404,9 @@ class Field:
             self.mesh, dim=self.nvdims, value=self.data * other, units=self.units
         )
 
+    def __rmul__(self, other):
+        return self * other
+
     def __truediv__(self, other):
         if isinstance(other, self.__class__):
             self.is_same_mesh_field(other)
@@ -405,6 +414,15 @@ class Field:
 
         return self.__class__(
             self.mesh, dim=self.nvdims, value=self.data / other, units=self.units
+        )
+
+    def __rtruediv__(self, other):
+        if isinstance(other, self.__class__):
+            self.is_same_mesh_field(other)
+            other = other.data
+
+        return self.__class__(
+            self.mesh, dim=self.nvdims, value=other / self.data, units=self.units
         )
 
     def __pow__(self, other):
