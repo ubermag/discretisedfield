6aa0530335e8a60067a1703270a4470b7cb36982
Martin Lang <m.lang@soton.ac.uk>
2022-11-23 16:30:47 +0100

    Remove DValue

--- discretisedfield/tests/test_operators.py
+++ discretisedfield/tests/test_operators.py
@@ -3,21 +3,6 @@ import numpy as np
 import discretisedfield as df
 
 
-def test_instances():
-    p1 = (0, 0, 0)
-    p2 = (100, 200, 300)
-    cell = (1, 2, 3)
-    region = df.Region(p1=p1, p2=p2)
-    mesh = df.Mesh(region=region, cell=cell)
-    field = df.Field(mesh, dim=3, value=(1, 2, 3))
-
-    assert df.dx(field) == 1
-    assert df.dy(field) == 2
-    assert df.dz(field) == 3
-    assert np.allclose(df.dS(field.plane("z")).mean(), (0, 0, 2))
-    assert df.dV(field) == 6
-
-
 def test_integrate():
     p1 = (0, 0, 0)
     p2 = (10, 10, 10)
