7d7add2ee07bef369e5e93f1a648c3d8be391f61
Sam Holt <s.j.r.holt@soton.ac.uk>
2022-11-29 16:42:59 +0000

    fix norm test

--- discretisedfield/tests/test_field.py
+++ discretisedfield/tests/test_field.py
@@ -366,18 +366,12 @@ class TestField:
         mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 10), cell=(5, 5, 5))
         f = df.Field(mesh, nvdim=3, value=(2, 2, 2))
 
-        assert np.all(f.norm.value == 2 * np.sqrt(3))
         assert np.all(f.norm.array == 2 * np.sqrt(3))
         assert np.all(f.array == 2)
 
         f.norm = 1
-        assert np.all(f.norm.value == 1)
         assert np.all(f.norm.array == 1)
         assert np.all(f.array == 1 / np.sqrt(3))
-
-        f.array[0, 0, 0, 0] = 3
-        assert isinstance(f.norm.value, np.ndarray)
-        assert not np.all(f.norm.value == 1)
 
         for mesh in self.meshes:
             for value, dtype in self.iters + self.vfuncs:
