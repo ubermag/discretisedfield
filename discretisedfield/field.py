Fix calculation of cumulative integral

discretisedfield/field.py
@@ -2461,8 +2461,11 @@ class Field:
 
         axis = mesh.region.dims.index(direction)
         if cumulative:
-            res_array = self.array * mesh.cell[axis] / 2
-            res_array[1:] += np.cumsum(self.array, axis=axis)[:-1] * mesh.cell[axis]
+            tmp_array = self.array / 2
+            left_cells = dfu.assemble_index(slice(None), 3, {axis: slice(None, -1)})
+            right_cells = dfu.assemble_index(slice(None), 3, {axis: slice(1, None)})
+            tmp_array[right_cells] += np.cumsum(self.array, axis=axis)[left_cells]
+            res_array = tmp_array * mesh.cell[axis]
         else:
             # NOTE reduce dimension n -> n-1:
             # - remove keepdims

discretisedfield/tests/test_field.py
@@ -1738,13 +1738,10 @@ class TestField:
         assert np.allclose(f_int((10, 10, 10)), (9.75, 9.75, 9.75))
         assert np.allclose(f_int.diff("x").array, f.array)
 
-        f = df.Field(mesh, nvdim=1, value=lambda p: p[0])
-        f_int = f.integrate(direction="x", cumulative=True)
-        assert np.allclose(f_int.diff("x").array, f.array)
-
-        f = df.Field(mesh, nvdim=1, value=lambda p: p[0] ** 2)
-        f_int = f.integrate(direction="x", cumulative=True)
-        assert np.allclose(f_int.diff("x").array, f.array)
+        for i, d in enumerate("xyz"):
+            f = df.Field(mesh, nvdim=1, value=lambda p: p[i])
+            assert np.allclose(f.integrate(d, cumulative=True).diff(d).array, f.array)
+            assert np.allclose(f.diff(d).integrate(d, cumulative=True).array, f.array)
 
         # Exceptions
         with pytest.raises(ValueError):