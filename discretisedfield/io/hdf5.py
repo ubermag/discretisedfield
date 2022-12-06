91b1601bd55413a916b6fd8a4d80f4c1e5853209
Martin Lang <m.lang@soton.ac.uk>
2022-12-06 13:39:49 +0100

    Additional docstrings and partial type hints for hdf5 field io

--- discretisedfield/io/hdf5.py
+++ discretisedfield/io/hdf5.py
@@ -10,17 +10,17 @@ import discretisedfield as df
 class _RegionIO_HDF5:
     _h5_attrs = ("pmin", "pmax", "dims", "ndim", "units", "tolerance_factor")
 
-    def _h5_save(self, h5_region):
+    def _h5_save(self, h5_region: h5py.Group):
         for attr in self._h5_attrs:
             h5_region.attrs[attr] = getattr(self, attr)
 
     @classmethod
-    def _h5_load(cls, h5_region):
+    def _h5_load(cls, h5_region: h5py.Group):
         return cls(**{attr: h5_region.attrs[attr] for attr in cls._h5_attrs})
 
 
 class _MeshIO_HDF5:
-    def _h5_save(self, h5_mesh):
+    def _h5_save(self, h5_mesh: h5py.Group):
         h5_region = h5_mesh.create_group("region")
         self.region._h5_save(h5_region)
 
@@ -35,7 +35,7 @@ class _MeshIO_HDF5:
             h5_mesh_subregions[i] = [*subregion.pmin, *subregion.pmax]
 
     @classmethod
-    def _h5_load(cls, h5_mesh):
+    def _h5_load(cls, h5_mesh: h5py.Group):
         region = df.Region._h5_load(h5_mesh["region"])
         subregions = {
             name.decode("utf-8"): df.Region(
@@ -48,6 +48,7 @@ class _MeshIO_HDF5:
 
 class _FieldIO_HDF5:
     def _to_hdf5(self, filename):
+        """Save a single field in a new hdf5 file."""
         utc_now = datetime.datetime.utcnow().isoformat(timespec="seconds")
         with h5py.File(filename, "w") as f:
             for attribute, value in [
@@ -66,7 +67,11 @@ class _FieldIO_HDF5:
             )
             self._h5_save_data(h5_field_data, slice(None))
 
-    def _h5_save_structure(self, h5_field):
+    def _h5_save_structure(self, h5_field: h5py.Group):
+        """
+        Save the 'field structure', that is the mesh and field attributes into an
+        existing hdf5 group. The field data is NOT saved.
+        """
         # TODO better method name?
         h5_mesh = h5_field.create_group("mesh")
         self.mesh._h5_save(h5_mesh)
@@ -76,11 +81,18 @@ class _FieldIO_HDF5:
             h5_field.attrs["vdims"] = self.vdims
         h5_field.attrs["unit"] = str(self.unit)
 
-    def _h5_save_data(self, h5_field_data, location):
+    def _h5_save_data(self, h5_field_data: h5py.Dataset, location):
+        """
+        Save field data into an existing hdf5 dataset at a given `location` inside that
+        dataset. For a single field in that dataset the `location` refers to the whole
+        dataset (`slice(None)`). Other values for `location` are useful to save a single
+        field into a bigger dataset, e.g. a dataset meant to contain a time series.
+        """
         h5_field_data[location] = self.array
 
     @classmethod
     def _from_hdf5(cls, filename):
+        """Read an hdf5 file containing a single Field object."""
         with h5py.File(filename, "r") as f:
             if "ubermag-hdf5-file-version" not in f.attrs:
                 return cls._h5_legacy_load_field(f, filename)
@@ -92,7 +104,13 @@ class _FieldIO_HDF5:
             return cls._h5_load_field(f["field"], slice(None))
 
     @classmethod
-    def _h5_load_field(cls, h5_field, data_location):
+    def _h5_load_field(cls, h5_field: h5py.Group, data_location):
+        """
+        Load a Field from an hdf5 group containing a single field. The hdf5 dataset
+        containing the field data can contain multiple fields arrays on the same mesh
+        (e.g. in a time series). The correct part of the array can be selected with
+        `data_location`.
+        """
         vdims = h5_field.attrs["vdims"] if h5_field.attrs["nvdim"] > 1 else None
         return cls(
             mesh=df.Mesh._h5_load(h5_field["mesh"]),
@@ -103,7 +121,7 @@ class _FieldIO_HDF5:
         )
 
     @classmethod
-    def _h5_legacy_load_field(cls, h5_file, filename):
+    def _h5_legacy_load_field(cls, h5_file: h5py.Group, filename):
         # reads old hdf5 files written prior to introducing ubermag-hdf5-file-version
         p1 = h5_file["field/mesh/region/p1"]
         p2 = h5_file["field/mesh/region/p2"]
