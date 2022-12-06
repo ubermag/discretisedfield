a8bd5d93d622cc14637dbfabdba93fe4ad34cc46
Martin Lang <m.lang@soton.ac.uk>
2022-12-06 09:54:56 +0100

    Review docstrings for to_file and from_file

--- discretisedfield/io/__init__.py
+++ discretisedfield/io/__init__.py
@@ -45,56 +45,55 @@ class _FieldIO(_FieldIOHDF5, _FieldIOOVF, _FieldIOVTK):
     ):
         """Write the field to OVF, HDF5, or VTK file.
 
-        If the extension of ``filename`` is ``.vtk``, a VTK file is written. The
-        representation of the data (``'bin'`` [``'bin8'`` as equivalent], ``'txt'``, or
-        ``'xml'``) is passed as ``'representation'``.
-        The data is saved as a ``RECTILINEAR_GRID`` dataset. Scalar field
-        (``dim=1``) is saved as ``SCALARS``. On the other hand, vector field
-        (``dim=3``) is saved as both ``VECTORS`` as well as ``SCALARS`` for all
-        three components to enable easy colouring of vectors in some
-        visualisation packages. The data is stored as ``CELL_DATA``.
-        The saved VTK file can be opened with `Paraview
-        <https://www.paraview.org/>`_ or `Mayavi
-        <https://docs.enthought.com/mayavi/mayavi/>`_. To show contour lines in
-        Paraview one has to first convert Cell Data to Point Data using a
-        filter.
+        For ``.ovf``, ``.omf``, or ``.ohf`` extensions the field is saved in an OVF
+        file. Possible values for `representation` of the data are ``'bin4'``,
+        ``'bin8'``, or ``'txt'``. If ``extend_scalar=True``, a scalar field will be
+        saved as a vector field. More precisely, if the value at a cell is X, that cell
+        will be saved as (X, 0, 0). Subregions are automatically saved in a separate
+        json file for ``save_subregions=True``.
 
+        If the extension of `filename` is ``.vtk``, a VTK file is written. Possible
+        values for `representation` are ``'bin'`` (``'bin8'`` as an equivalent for
+        convenience), ``'txt'``, or ``'xml'``. The data is saved as a
+        ``RECTILINEAR_GRID``. A scalar field (``nvdim=1``) is saved as ``SCALARS``. A
+        vector field (``nvdim=3``) is saved as both ``VECTORS`` as well as ``SCALARS``
+        for all three components to enable easier colouring of vectors in some
+        visualisation packages. The data is stored as ``CELL_DATA``. Subregions are
+        automatically saved in a separate json file for ``save_subregions=True``.
+        `extend_scalar` has no effect for VTK files. The saved VTK file can be opened
+        with `Paraview <https://www.paraview.org/>`_ or `Mayavi
+        <https://docs.enthought.com/mayavi/mayavi/>`_. To show contour lines in Paraview
+        one has to first convert Cell Data to Point Data using a filter.
 
-        For ``.ovf``, ``.omf``, or ``.ohf`` extensions, the field is saved to OVF file.
-        In that case, the representation of data (``'bin4'``, ``'bin8'``, or ``'txt'``)
-        is passed as ``representation`` and if ``extend_scalar=True``, a scalar field
-        will be saved as a vector field. More precisely, if the value at a cell is X,
-        that cell will be saved as (X, 0, 0).
-
-        Finally, if the extension of ``filename`` is ``.hdf5``, HDF5 file will be
-        written. The format of the HDF5 files was changed in discretisedfield version
-        0.70.0.
+        If the extension of `filename` is ``.hdf5`` or ``.h5`` an HDF5 file will be
+        written. The parameters `representation`, `extend_scalar` and `save_subregions`
+        have no effect for HDF5 files and are silently ignored. Subregions are stored
+        inside the HDF5 file.
 
         Parameters
         ----------
         filename : str
 
-            Name of the file written.
+            Name of the file written. The suffix determines the file type.
 
         representation : str, optional
 
-            Only supported for OVF and VTK files. In the case of OVF files
-            (``.ovf``, ``.omf``, or ``.ohf``) the representation can be
-            ``'bin4'``, ``'bin8'``, or ``'txt'``. For VTK files (``.vtk``) the
-            representation can be ``bin``, ``xml``, or ``txt``. Defaults to
-            ``'bin8'`` (interpreted as ``bin`` for VTK files).
+            Only supported for OVF and VTK files. In the case of OVF files (``.ovf``,
+            ``.omf``, or ``.ohf``) the representation can be ``'bin4'``, ``'bin8'``, or
+            ``'txt'``. For VTK files (``.vtk``) the representation can be ``bin``
+            (``bin8``), ``xml``, or ``txt``. Defaults to ``'bin8'`` (interpreted as
+            ``bin`` for VTK files).
 
         extend_scalar : bool, optional
 
-            If ``True``, a scalar field will be saved as a vector field. More
-            precisely, if the value at a cell is 3, that cell will be saved as
-            (3, 0, 0). This is valid only for the OVF file formats. Defaults to
-            ``False``.
+            If ``True``, a scalar field will be saved as a vector field. More precisely,
+            if the value at a cell is 3, that cell will be saved as (3, 0, 0). This is
+            valid only for the OVF file formats. Defaults to ``False``.
 
         save_subregions : bool, optional
 
             If ``True`` and subregions are defined for the mesh the subregions will be
-            saved to a json file. Defaults to ``True``. This has no effect for hdf5
+            saved to a json file. Defaults to ``True``. This has no effect for HDF5
             files which always contain subregions in the file.
 
         See also
@@ -103,7 +102,7 @@ class _FieldIO(_FieldIOHDF5, _FieldIOOVF, _FieldIOVTK):
 
         Example
         -------
-        1. Write field to the OVF file.
+        1. Write field to an OVF file.
 
         >>> import os
         >>> import discretisedfield as df
@@ -123,15 +122,18 @@ class _FieldIO(_FieldIOHDF5, _FieldIOOVF, _FieldIOVTK):
         True
         >>> os.remove(filename)  # delete the file
 
-        2. Write field to the VTK file.
+        2. Write field to a VTK file.
 
         >>> filename = 'mytestfile.vtk'
         >>> field.to_file(filename)  # write the file
         >>> os.path.isfile(filename)
         True
+        >>> field_read = df.Field.from_file(filename)  # read the file
+        >>> field_read == field
+        True
         >>> os.remove(filename)  # delete the file
 
-        3. Write field to the HDF5 file.
+        3. Write field to an HDF5 file.
 
         >>> filename = 'mytestfile.hdf5'
         >>> field.to_file(filename)  # write the file
@@ -151,14 +153,14 @@ class _FieldIO(_FieldIOHDF5, _FieldIOOVF, _FieldIOVTK):
                 extend_scalar=extend_scalar,
                 save_subregions=save_subregions,
             )
-        elif filename.suffix in [".hdf5", ".h5"]:
-            self._to_hdf5(filename)
         elif filename.suffix == ".vtk":
             self._to_vtk(
                 filename,
                 representation=representation,
                 save_subregions=save_subregions,
             )
+        elif filename.suffix in [".hdf5", ".h5"]:
+            self._to_hdf5(filename)
         else:
             raise ValueError(
                 f"Writing file with extension {filename.suffix} not supported."
@@ -170,32 +172,37 @@ class _FieldIO(_FieldIOHDF5, _FieldIOOVF, _FieldIOVTK):
 
     @classmethod
     def from_file(cls, filename):
-        """Read the field from an OVF (1.0 or 2.0), VTK, or HDF5 file.
+        """Read a field from an OVF (1.0 or 2.0), VTK, or HDF5 file.
 
-        The extension of the ``filename`` should correspond to either:
-            - OVF (``.ovf``, ``.omf``, ``.ohf``, ``.oef``)
-            - VTK (``.vtk``), or
-            - HDF5 (``.hdf5`` or ``.h5``).
+        The extension of `filename` can be:
+            - ``.ovf``, ``.omf``, ``.ohf`` or ``.oef`` for OVF files,
+            - ``.vtk`` for VTK files, or
+            - ``.hdf5`` or ``.h5`` for HDF5 files.
 
-        This method automatically determines the file type based on the file name
-        extension.
+        This method automatically determines the file type based on the extension of
+        `filename`.
 
         For OVF the data representation (``txt``, ``bin4``, or ``bin8``) as well as the
-        OVF version (OVF1.0 or OVF2.0) are extracted from the file itself.
+        OVF version (OVF1.0 or OVF2.0) are extracted from the file itself. Mesh
+        subregions are loaded from a separate json file if it exists.
 
-        For VTK files this method reads the field from a VTK file defined on RECTILINEAR
-        GRID written by ``discretisedfield.to_file``. It expects the data do be
-        specified as cell data and one (vector) field with the name ``field``. A vector
-        field should also contain data for the individual components. The individual
-        component names are used as ``vdims`` for the new field. They must appear in the
-        form ``<componentname>-component``. Older versions of discretisedfield did write
-        the data as point data instead of cell data. This function can load new and old
-        files and automatically extracts the correct data without additional user input.
+        For VTK files this method reads the field from a VTK file containing a
+        ``RECTILINEAR_GRID`` written by ``discretisedfield.to_file``. It expects the
+        data do be specified as cell data and one (vector) field with the name
+        ``field``. A vector field should also contain data for the individual
+        components. The individual component names are used as ``vdims`` for the new
+        field. They must appear in the form ``<componentname>-component``. Older
+        versions of discretisedfield did write the data as point data instead of cell
+        data. This function can load new and old files and automatically extract the
+        correct data without additional user input. Mesh subregions are loaded from a
+        separate json file if it exists.
 
-        The file format for hdf5 files was changed in discretisedfield version 0.70.0.
-        Older versions of discretisedfield did only did not save all attributes (e.g. no
-        subregions). Reading old files is automatically handled internally. Subregions
-        can only be read from disk if they were saved in a separate json file.
+        For HDF5 files written with discretisedfield version 0.70.0 or newer all data is
+        contained in the file. No separate json file for subregions is read. Older
+        versions of discretisedfield did only did not save all attributes (e.g. no
+        subregions). Reading old files is automatically handled internally. For old HDF5
+        files subregions can be read from disk if they were saved in a separate json
+        file.
 
         Parameters
         ----------
@@ -221,20 +228,20 @@ class _FieldIO(_FieldIOHDF5, _FieldIOOVF, _FieldIOVTK):
         >>> import discretisedfield as df
         ...
         >>> dirname = os.path.join(os.path.dirname(__file__),
-        ...                        'tests', 'test_sample')
+        ...                        '..', 'tests', 'test_sample')
         >>> filename = os.path.join(dirname, 'oommf-ovf2-bin4.omf')
         >>> field = df.Field.from_file(filename)
         >>> field
         Field(...)
 
-        2. Read a field from the VTK file.
+        2. Read a field from a VTK file.
 
         >>> filename = os.path.join(dirname, 'vtk-file.vtk')
         >>> field = df.Field.from_file(filename)
         >>> field
         Field(...)
 
-        3. Read a field from the HDF5 file.
+        3. Read a field from an HDF5 file.
 
         >>> filename = os.path.join(dirname, 'hdf5-file.hdf5')
         >>> field = df.Field.from_file(filename)
