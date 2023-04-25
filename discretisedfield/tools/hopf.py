f90cdac4f3716961c57a00f72e44219a8b7034bd
Ross Knapman <rjknapman@gmail.com>
2023-04-25 14:41:42 +0200

    Write docstring for Hopf index

--- discretisedfield/tools/hopf.py
+++ discretisedfield/tools/hopf.py
@@ -4,14 +4,64 @@ import discretisedfield.tools as dft
 
 
 def hopf_index(field):
-    """TODO Docstring"""
+    r"""Hopf index.
+
+    Hopf index for a (magnetic) unit vector field :math:`\boldsymbol{m}`
+    is defined as:
+
+    .. math::
+
+        H = -\frac{1}{(8\pi)^2} \int \mathrm{d}^3 r \, \boldsymbol{F} \cdot
+        \boldsymbol{A},
+
+    where :math:`boldsymbol{F}` is the emergent magnetic field
+    :math:`F_i = \varepsilon_{ijk} \boldsymbol{m} \cdot (\partial_j \boldsymbol{m}
+    \times \partial_k \boldsymbol{m})`
+    and :math:`\boldsymbol{A}` is defined implicitly through :math:`\nabla \times
+    \boldsymbol{A} = \boldsymbol{F}`.
+
+    Parameters
+    ----------
+    field : discretisedfield.Field
+
+        Vector field.
+
+    Returns
+    -------
+    float
+
+        Hopf index.
+
+    Raises
+    ------
+    ValueError
+
+        If the field is not three-dimensional.
+
+    Example
+    -------
+    1. Compute Hopf index of a spatially constant vector field.
+
+    >>> import discretisedfield as df
+    >>> import discretisedfield.tools as dft
+    ...
+    >>> p1 = (0, 0, 0)
+    >>> p2 = (10, 10, 10)
+    >>> cell = (2, 2, 2)
+    >>> mesh = df.Mesh(p1=p1, p2=p2, cell=cell)
+    >>> f = df.Field(mesh, nvdim=3, value=(1, 1, -1))
+    ...
+    >>> dft.hopf_index(f)
+    Field(...)
+
+    """
 
     if field.nvdim != 3:
         raise ValueError(f"Cannot compute Hopf index for {field.nvdim=} field.")
 
     if field.mesh.region.ndim != 3:
         raise ValueError(
-            "The Hopf indexcan only be computed on fields with 3"
+            "The Hopf index can only be computed on fields with 3"
             f" spatial dimensions, not {field.mesh.region.ndim=}."
         )
 
