7d37be095474635254d417cf74384abfd9180f3b
Samuel Holt <samjrholt@gmail.com>
2023-08-02 17:58:55 +0200

    ifft docs

--- discretisedfield/mesh.py
+++ discretisedfield/mesh.py
@@ -2170,26 +2170,36 @@ class Mesh(_MeshIO):
         return mesh
 
     def ifftn(self, rfft=False, shape=None):
-        """N dimensional discrete inverse FFT of the mesh.
+        """Performs an N-dimensional discrete inverse Fast Fourier Transform (iFFT)
+        on the mesh.
 
-        If rfft is ``True`` and shape is ``None``, the shape of the original mesh
-        is assumed to be even in the last dimension.
+        This function calculates the iFFT in an N-dimensional space. The iFFT is a
+        method to convert a frequency-domain signal into a spatial-domain signal.
+        If 'rfft' is set to True and 'shape' is None, the original mesh shape is
+        assumed to be even in the last dimension.
+
+        Please note that during Fourier transformations, the original position
+        information is lost, causing the inverse Fourier transform to be centered at
+        the origin. This can be rectified by `mesh.translate` to translate the mesh
+        back to the desired position.
 
         Parameters
         ----------
         rfft : bool, optional
 
-            If ``True``, the a real FFT is performed. Defaults to ``False``.
+            If set to True, a real FFT is performed. If False, a complex FFT is
+            performed. Defaults to False.
 
         shape : (tuple, np.ndarray, list), optional
 
-            Shape of the original mesh. Defaults to ``None``.
+            Specifies the shape of the original mesh. Defaults to None, which means the
+            shape of the original mesh is used.
 
         Returns
         -------
         discretisedfield.Mesh
 
-            Inverse Fourier transform of the mesh.
+            A mesh representing the inverse Fourier transform of the mesh.
 
         Examples
         --------
@@ -2216,6 +2226,29 @@ class Mesh(_MeshIO):
         array([-5.])
         >>> ifft_mesh.region.pmax
         array([5.])
+
+        3. Perform a 2D iFFT.
+        >>> mesh = df.Mesh(p1=(0, 0), p2=(10, 10), cell=(2, 2))
+        >>> ifft_mesh = mesh.fftn().ifftn()
+        >>> ifft_mesh.n
+        array([5, 5])
+        >>> ifft_mesh.cell
+        array([2., 2.])
+        >>> ifft_mesh.region.pmin
+        array([-5., -5.])
+        >>> ifft_mesh.region.pmax
+        array([5., 5.])
+
+        4. Perform a real 2D iFFT.
+        >>> ifft_mesh = mesh.fftn(rfft=True).ifftn(rfft=True, shape=mesh.n)
+        >>> ifft_mesh.n
+        array([5, 5])
+        >>> ifft_mesh.cell
+        array([2., 2.])
+        >>> ifft_mesh.region.pmin
+        array([-5., -5.])
+        >>> ifft_mesh.region.pmax
+        array([5., 5.])
 
         """
         if shape is not None:
