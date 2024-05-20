915f1bf4021efa051da0cb144b7ff793f9f9cbc0
Martin Lang <martin.lang@mpsd.mpg.de>
2024-05-20 10:03:27 +0200

    Redundant import aliases in __init__ to avoid marking imports as unused
    Suggested here: https://docs.astral.sh/ruff/rules/unused-import/

--- discretisedfield/util/__init__.py
+++ discretisedfield/util/__init__.py
@@ -1,2 +1,4 @@
-# ruff: noqa: F401
-from .util import array2tuple, assemble_index, bergluescher_angle, rescale_xarray
+from .util import array2tuple as array2tuple
+from .util import assemble_index as assemble_index
+from .util import bergluescher_angle as bergluescher_angle
+from .util import rescale_xarray as rescale_xarray
